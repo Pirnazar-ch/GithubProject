304435055728c163f21cfc5d30aaaae69c845454
Pirnazar-ch <pirnazarchoriyev@gmail.com>
2025-07-22 10:39:36 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+print("Hello World")
+
+# Pycharm + Github
+"""
+git config --global user.name "Pirnazar-ch"
+git config --global user.email "pirnazarchoriyev@gmail.com"
+"""
+
+"""
+git initc
+git add .
+git commit -m "first commit"
+git branch -M master
+git remote add origin https://github.com/Pirnazar-ch/GithubProject.git
+git push -u origin master
+
+"""
